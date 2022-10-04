98468f74998d987256c6fa3b1356cd54d797db5b
oniondelta <deltazonetw@gmail.com>
2022-10-04 10:38:36 +0800

    精簡寫法

--- lua/processor_kr_2set_0m.lua
+++ lua/processor_kr_2set_0m.lua
@@ -4,63 +4,56 @@
 韓語 2set 零選項方案專用，使選單變為零。
 --]]
 
-local set_char = Set {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" } --> {a=true,b=true...}
-local set_number = Set {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0" }
+
+local set_char = Set {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" , "Q", "W", "E", "R", "T" ,"O" ,"P"}  --> {a=true,b=true...}
+local set_number = Set {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+
 
 local function kr_2set_0m(key,env)
   local engine = env.engine
   local context = engine.context
-  local ascii_m = context:get_option("ascii_mode")
 
-  local function check_qwertop()
-    if key:eq(KeyEvent("Shift+Q")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+W")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+E")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+R")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+T")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+O")) then
-      return true
-    elseif key:eq(KeyEvent("Shift+P")) then
-      return true
-    else
-      return false
-    end
-  end
+  --- pass ascii_mode
+  if context:get_option('ascii_mode') then
+    return 2
 
-  local check_prefix = string.find(context.input, '=[a-z]*$')  --match
+  --- pass release ctrl alt super
+  elseif key:release() or key:ctrl() or key:alt() or key:super() then
+    return 2
 
-  if (not ascii_m) and set_char[key:repr()] and (not check_prefix) or (not ascii_m) and check_qwertop() and (not check_prefix) then
-    local addend = string.gsub(key:repr(), 'Shift%+', '')
+  --- pass reverse_lookup prefix （使反查鍵可展示全部選項）
+  elseif string.find(context.input, '=[a-z]*$') then
+    return 2
+
+  --- 修正「Shift+Return」commit_raw_input 設定失效問題
+  elseif key:eq(KeyEvent("Shift+Return")) and (context:is_composing()) then
+    engine:commit_text(context.input)
+    context:clear()
+    return 1
+
+  --- 《最主要部分》使 [a-zQWERTOP] 組字且半上屏
+  -- local asciikeys = string.char(key.keycode)
+  elseif set_char[string.char(key.keycode)] then
     context:reopen_previous_segment()
-    context.input = context.input .. addend
+    context.input = context.input .. string.char(key.keycode)
     context:confirm_current_selection()
     return 1
 
-  -- 修正「;」使可展示選單
-  elseif (not ascii_m) and key:repr() == 'semicolon' then
+  --- 修正尾綴「;」出漢字，使其可展示選單
+  elseif key:repr() == 'semicolon' then
     context:reopen_previous_segment()
     context.input = context.input .. ';'
     return 1
 
-  -- 修正「數字」不能直接上屏問題
-  elseif (not ascii_m) and set_number[key:repr()] and (not context:is_composing()) then
+  --- 修正「數字」不能直接上屏問題
+  elseif set_number[key:repr()] and (not context:is_composing()) then
     engine:commit_text(key:repr())
     -- context:clear()
-    return 1
-
-  -- 修正「Shift+Return」失效問題
-  elseif (not ascii_m) and key:eq(KeyEvent("Shift+Return")) and (context:is_composing()) then
-    engine:commit_text(context.input)
-    context:clear()
     return 1
 
   end
   return 2
 end
+
 
 return kr_2set_0m
