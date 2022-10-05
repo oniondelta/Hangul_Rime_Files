145d4c8b81d1ba8f358511172c114fe0846a42cc
oniondelta <deltazonetw@gmail.com>
2022-10-05 16:00:04 +0800

    相關lua修改

--- rime.lua
+++ rime.lua
@@ -1,10 +1,11 @@
 
---- kr_2set_0m （hangeul2set_zeromenu）
--- 韓語遮屏成零選項。
--- 只有 hangeul2set_zeromenu 方案有用到。
-kr_2set_0m = require("processor_kr_2set_0m")
-
 
 --- kr_hnc_1m_filter （hangeul_hnc）
 -- 韓語遮屏只剩一個選項。開關（kr_1m）
 kr_hnc_1m_filter =  require("filter_kr_hnc_1m_filter")
+
+
+--- kr_2set_0m _choice（hangeul2set_zeromenu）
+-- 韓語成零選項。開關（space_mode）、開關（kr_0m）
+kr_2set_0m_choice = require("processor_kr_2set_0m_choice")
+
