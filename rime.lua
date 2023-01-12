e1ba59780cd6eba7dfa41d7373e6b6026e3774be
oniondelta <deltazonetw@gmail.com>
2023-01-12 15:45:54 +0800

    修

--- rime.lua
+++ rime.lua
@@ -2,7 +2,7 @@
 
 --- kr_hnc_1m_filter （hangeul_hnc）
 -- 韓語遮屏只剩一個選項。開關（kr_1m）
-kr_hnc_1m_filter =  require("filter_kr_hnc_1m_filter")
+kr_hnc_1m_filter = require("filter_kr_hnc_1m_filter")
 
 
 --- kr_2set_0m_choice （hangeul2set_zeromenu）
